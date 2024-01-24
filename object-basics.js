be528168909f79061c8f6f99841f48f6ce59d796
Tanha <nusrattanha897@gmail.com>
2024-01-24 18:28:00 +0600

    object value accessing and symbol variable as key in object

--- object-basics.js
+++ object-basics.js
@@ -3,9 +3,18 @@
 // declaring a object
 // topic-01: object literals
 // const jsUser = {}; //empty object
+
+//todo: declare a symbol variable. Use it as key in object and print the output.
+//declare a Symbol variable
+const mySym = Symbol('key1');
+
 const jsUser = {
     //key value pair
     name: "Nusrat", 
+    //key is considered as a string.
+    "full name": "Nusrat Tanha",
+    // declaring symbol variable as key
+    [mySym] : 'myKey1',
     age: 24,
     email: 'nusrat@gmail.com',
     location: 'Mirpur,Dhaka,Bangladesh',
@@ -14,7 +23,16 @@ const jsUser = {
 
 };
 
+// accessing object values
+console.log(jsUser.email);
+console.log(jsUser["email"]);
+// console.log(jsUser.full name); // not possible to access this js object using dot notation
+console.log(jsUser["full name"]); //imp: this is recommended
+//accessing the symbol value
+console.log(typeof jsUser[mySym]);
+
+
+
 //topic-02: using constructor
 // Object.create()
 const newUser = new Object();
-console.log(newUser);
